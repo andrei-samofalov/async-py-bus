f4f8d9708137fad849d4042b349c77dc89a7ded9
andrei-samofalov <andrei.e.samofalov@gmail.com>
2025-01-27 00:18:58 +0300

    import fix

--- src/pybus/__init__.py
+++ src/pybus/__init__.py
@@ -1,12 +1,12 @@
-from base.dispatcher import Dispatcher as Dispatcher
-from base.dispatcher import default_dispatcher as dispatcher
-from base.engine.events import EventEngine as EventEngine
-from base.engine.requests import RequestEngine as RequestEngine
-from base.handlers import AbstractHandler as AbstractHandler
-from base.maps import EventHandlerMap as EventHandlerMap
-from base.maps import RequestHandlerMap as RequestHandlerMap
-from base.routers.eventrouter import EventRouter as EventRouter
-from base.routers.requestrouter import RequestRouter as RequestRouter
+from pybus.base.dispatcher import Dispatcher as Dispatcher
+from pybus.base.dispatcher import default_dispatcher as dispatcher
+from pybus.base.engine.events import EventEngine as EventEngine
+from pybus.base.engine.requests import RequestEngine as RequestEngine
+from pybus.base.handlers import AbstractHandler as AbstractHandler
+from pybus.base.maps import EventHandlerMap as EventHandlerMap
+from pybus.base.maps import RequestHandlerMap as RequestHandlerMap
+from pybus.base.routers.eventrouter import EventRouter as EventRouter
+from pybus.base.routers.requestrouter import RequestRouter as RequestRouter
 
 __all__ = [
     "AbstractHandler",
